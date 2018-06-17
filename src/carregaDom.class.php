f67bb384e4e59cab813ec9dd8bca3ed1b8bbeacb
paulosergioduff <paulosergioduff@hotmail.com>
2018-06-17 17:09:21 -0300

    GitFlash

--- src/carregaDom.class.php
+++ src/carregaDom.class.php
@@ -1,77 +1,72 @@
 <?php
-
 libxml_use_internal_errors(true); // Desativa mensagens de duplicada de DOM
-
-class imprimeDom{
-	public $caminho;
-	public $tag;
-	public $parametro;
-	//$this->caminho = $caminho;
-	
-	public function DIPJS($caminho)
-	{
-
-		$dom = new DOMDocument();
-		$dom->loadHTMLFile($caminho);
-
-		// Consultando os links
-		$links = $dom->getElementsByTagName('script');
-		foreach ($links as $link) {
-			
-		  echo "<scrip src='".$link->getAttribute('src')."'>".PHP_EOL;
-		    
-		}
-				
-				
-			}
-
-		public function DIPCSS($caminho)
-	{
-
-		$dom = new DOMDocument();
-		$dom->loadHTMLFile($caminho);
-
-		// Consultando os links
-		$links = $dom->getElementsByTagName('link');
-		foreach ($links as $link) {
-			
-		   echo "<link rel='stylesheet' href='".$link->getAttribute('href')."'>".PHP_EOL;
-
-		    
-		}
-				
-				
-			}
-
-
-			public function inputCommomClass($caminho, $tag)
-	{
-
-		$dom = new DOMDocument();
-		$dom->loadHTMLFile($caminho);
-
-		$controle = 0; // Variável de controle
-		// Consultando os links
-		$links = $dom->getElementsByTagName($tag);
-		foreach ($links as $link) {
-		
-		if ($controle == 0) { // Pega primeiro valor da class e assume como padrão
-				$this->novaClass = '<input class='."'".$link->getAttribute('class')."' >"; // Retorna tag do HTML com a nova class
-				$this->retorno = " class='".$link->getAttribute('class').PHP_EOL."' ";
-				// Oferece ópção para troca de classe
-			}	
-		  
-		  $controle++;
-		    
-		}
-				
-				
-			}
+class imprimeDOM
+{
+				public $caminho;
+				public $tag;
+				public $parametro;
+				public $alvo;
+				public $modelo;
+				public $novoAtributo;
+				public $entrega;
+				public function DIPJS($caminho)
+				{
+								$dom = new DOMDocument();
+								$dom->loadHTMLFile($caminho);
+								// Consultando os links
+								$links = $dom->getElementsByTagName('script');
+								foreach ($links as $link) {
+												echo "<scrip src='" . $link->getAttribute('src') . "'>" . PHP_EOL;
+								}
+				}
+				public function DIPCSS($caminho)
+				{
+								$dom = new DOMDocument();
+								$dom->loadHTMLFile($caminho);
+								// Consultando os links
+								$links = $dom->getElementsByTagName('link');
+								foreach ($links as $link) {
+												echo "<link rel='stylesheet' href='" . $link->getAttribute('href') . "'>" . PHP_EOL;
+								}
+				}
+				public function inputCommomClass($caminho, $tag)
+				{
+								$dom = new DOMDocument();
+								$dom->loadHTMLFile($caminho);
+								$controle = 0; // Variável de controle
+								// Consultando os links
+								$links    = $dom->getElementsByTagName($tag);
+								foreach ($links as $link) {
+												if ($controle == 0) { // Pega primeiro valor da class e assume como padrão
+																$this->novaClass = '<input class=' . "'" . $link->getAttribute('class') . "' >"; // Retorna tag do HTML com a nova class
+																$this->retorno   = " class='" . $link->getAttribute('class') . PHP_EOL . "' ";
+																// Oferece ópção para troca de classe
+												}
+												$controle++;
+								}
+							}
+				public function CommomTag($caminho, $tag, $atributo)
+				{
+								$dom = new DOMDocument();
+								$dom->loadHTMLFile($caminho);
+								$controle = 0; // Variável de controle
+								// Consultando os links
+								$links    = $dom->getElementsByTagName($tag);
+								foreach ($links as $link) {
+												if ($controle == 0) { // Pega primeiro valor e assume como padrão
+																$this->novaClass = '<'.$tagN.' class=' . "'" . $link->getAttribute($atributo) . "' >"; // Retorna tag do HTML com a nova class
+																$this->retorno   = " class='" . $link->getAttribute($atributo) . PHP_EOL . "' ";
+																// Oferece ópção para troca de classe
+												}
+												$controle++;
+								}
+				}
+				public function entrega(){
+					$this->alvo = file_get_contents($this->alvo); // Pega todo conteúdo da página html alvo
+					$this->entrega = str_replace("$this->tag", "$this->tag $this->novoAtributo ", $this->alvo); // str_replace substitui atributo antigo
+					echo $this->entrega; // Imprime na tela o resultado da troca
+				}
 }
-
-
-
-
 ?>
 
 
--- src/exemploCarregaDom.php
+++ src/exemploCarregaDom.php
@@ -1,50 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php 
-
+<?php
 require_once "carregaDom.class.php";
-require_once "menu.class.php";
-
 $dip = new imprimeDom;
 $dip->DIPCSS('temas/bootstrap/modelo.html'); // Carrega CSSs
 $dip->DIPJS('temas/bootstrap/modelo.html'); // Carrega CSSs
-
 ?>
 	<title></title>
 </head>
 <body>
 
-<?php 
+<?php
+$app          = new imprimeDOM; // Instancia classe de captura de DOM
+$app->tag = $euQueroATag  = 'button';
+$app->alvo = $localizadaEm = 'html/form-alvo.html';
+$modelo = $app->modelo = 'temas/bootstrap/form-modelo-bootstrap.html'; // Página modelo para aplicar o template
+$app->inputCommomClass($modelo, $euQueroATag); // Usa uma página HTML como modelo e injeta dependências CSS na página
+$app->novoAtributo    = $app->retorno; // Determina novo atributo para a tag
 
-$euQueroATag = 'button';
-$localizadaEm = 'html/form-blank.html';
-
-	$app = new imprimeDom;
-	$app->inputCommomClass('temas/bootstrap/form.html', $euQueroATag); // Usa uma página HTML como modelo e injeta dependências CSS na página
-	$novaClassInput = $app->retorno;
-
-$seTransformeEm = $novaClassInput;
-$baseadoNoTemplate = 'temas/bootstrap/form.html'; // Formulário de exemplo (não é bootstrap)
+$app->entrega();
 
 
-$originalHTML = file_get_contents('html/form-blank.html');
-
-$novoHTML = str_replace("$euQueroATag", "$euQueroATag $novaClassInput ", $originalHTML);
-
-echo $novoHTML;
-
-
-echo "<ul>";
-
-$menu =  new menuGeral;
-
-$menu->tag = 'li';
-$menu->class = "$novaClassInput";
-
-echo $menu->geraMenu('Meu novo item', 'http://wwwgoogle.com');
-
-echo "</ul>";
 ?>
 
 </body>
