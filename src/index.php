0e8d5ae24ec2089c70e5f6a5f98c6cccd019a441
paulosergioduff <paulosergioduff@gmail.com>
2020-08-08 19:19:26 -0300

    Pegando dependencias com index

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once "carregaDom.class.php";
+$dip = new imprimeDom;
+
+ $dip->DIPCSS('temas/bigtires/recorte.html'); // Carrega CSSs
+ $dip->DIPJS('temas/bigtires/recorte.html'); // Carrega CSSs
+
+echo "<p>";
+
+?>
+
